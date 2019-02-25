846c43a8df8c63c17bcc228485a161a8e8cd130b
herr kaste <herr.kaste@gmail.com>
2019-02-25 12:08:36 +0100

    Auto-format test using 'black' tool

--- tests/testBracketGuard.py
+++ tests/testBracketGuard.py
@@ -3,64 +3,49 @@ from unittest import TestCase
 
 version = sublime.version()
 
+
 class TestBracketGuard(TestCase):
+    def setUp(self):
+        self.view = sublime.active_window().new_file()
+        self.view.settings().set("is_test", True)
 
-	def setUp(self):
+    def tearDown(self):
+        if self.view:
+            self.view.set_scratch(True)
+            self.view.close()
 
-		self.view = sublime.active_window().new_file()
-		self.view.settings().set("is_test", True)
+    def insertCodeAndGetRegions(self, code):
+        self.view.run_command("insert", {"characters": code})
+        return self.view.get_regions("BracketGuardRegions")
 
+    def testPureValidBrackets(self):
+        openerRegions = self.insertCodeAndGetRegions("([{}])")
+        self.assertEqual(len(openerRegions), 0)
 
-	def tearDown(self):
+    def testValidBracketsInCode(self):
+        openerRegions = self.insertCodeAndGetRegions("a(bc[defg{hijkl}mn])o")
+        self.assertEqual(len(openerRegions), 0)
 
-		if self.view:
-			self.view.set_scratch(True)
-			self.view.window().run_command("close_file")
+        openerRegions = self.insertCodeAndGetRegions("<a href={ url }>")
+        self.assertEqual(len(openerRegions), 0)
 
+    def testInvalidBracketsWrongCloser(self):
+        bracketGuardRegions = self.insertCodeAndGetRegions("({}])")
 
-	def insertCodeAndGetRegions(self, code):
+        self.assertEqual(len(bracketGuardRegions), 2)
+        self.assertEqual(bracketGuardRegions[0].a, 0)
+        self.assertEqual(bracketGuardRegions[1].a, 3)
 
-		self.view.run_command("insert", {"characters": code})
-		return self.view.get_regions("BracketGuardRegions")
+    def testInvalidBracketsNoCloser(self):
+        bracketGuardRegions = self.insertCodeAndGetRegions("({}")
 
+        self.assertEqual(len(bracketGuardRegions), 2)
+        self.assertEqual(bracketGuardRegions[0].a, -1)
+        self.assertEqual(bracketGuardRegions[1].a, 0)
 
-	def testPureValidBrackets(self):
+    def testInvalidBracketsNoOpener(self):
+        bracketGuardRegions = self.insertCodeAndGetRegions("){}")
 
-		openerRegions = self.insertCodeAndGetRegions("([{}])")
-		self.assertEqual(len(openerRegions), 0)
-
-
-	def testValidBracketsInCode(self):
-
-		openerRegions = self.insertCodeAndGetRegions("a(bc[defg{hijkl}mn])o")
-		self.assertEqual(len(openerRegions), 0)
-
-		openerRegions = self.insertCodeAndGetRegions("<a href={ url }>")
-		self.assertEqual(len(openerRegions), 0)
-
-
-	def testInvalidBracketsWrongCloser(self):
-
-		bracketGuardRegions = self.insertCodeAndGetRegions("({}])")
-
-		self.assertEqual(len(bracketGuardRegions), 2)
-		self.assertEqual(bracketGuardRegions[0].a, 0)
-		self.assertEqual(bracketGuardRegions[1].a, 3)
-
-
-	def testInvalidBracketsNoCloser(self):
-
-		bracketGuardRegions = self.insertCodeAndGetRegions("({}")
-
-		self.assertEqual(len(bracketGuardRegions), 2)
-		self.assertEqual(bracketGuardRegions[0].a, -1)
-		self.assertEqual(bracketGuardRegions[1].a, 0)
-
-
-	def testInvalidBracketsNoOpener(self):
-
-		bracketGuardRegions = self.insertCodeAndGetRegions("){}")
-
-		self.assertEqual(len(bracketGuardRegions), 2)
-		self.assertEqual(bracketGuardRegions[0].a, -1)
-		self.assertEqual(bracketGuardRegions[1].a, 0)
+        self.assertEqual(len(bracketGuardRegions), 2)
+        self.assertEqual(bracketGuardRegions[0].a, -1)
+        self.assertEqual(bracketGuardRegions[1].a, 0)
